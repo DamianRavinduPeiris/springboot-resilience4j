f526b381cfdfc9baca21c1a30b324390fe4fa384
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-10-08 11:42:29 +0530

    Initial Commit.

---
+++ src/main/java/com/damian/resilience4j/ServiceAResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.damian.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAResilience4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAResilience4jApplication.class, args);
+    }
+
+}
